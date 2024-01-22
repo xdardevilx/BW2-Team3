ed338b6c55c0dad7eac48c2e1896ac042307f044
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-01-22 14:53:02 +0100

    ok

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let myUrl = "https://striveschool-api.herokuapp.com/api/deezer/search?q=salmo";
+let myUrl = "https://striveschool-api.herokuapp.com/api/deezer/";
+let nomeArtista = "salmo";
+let searchArtista = "search?q=" + nomeArtista;
 
 const getDataDatas = function () {
-  fetch(myUrl)
+  fetch(myUrl + searchArtista)
     .then((response) => {
       if (response.ok) {
         return response.json();
@@ -11,9 +13,19 @@ const getDataDatas = function () {
     })
     .then((data) => {
       console.log(data, "data");
-      let dataArray = data;
-      const arrayAlbumId = Array.from(dataArray);
-      console.log(arrayAlbumId);
+      const oggettoConArray = data;
+      const arrayDiOggetti = oggettoConArray.data;
+      console.log(arrayDiOggetti);
+
+      const artistiStampati = new Set(); // TRACCIO GLI ARTISTI GIà STAMPATI
+      arrayDiOggetti.forEach((element) => {
+        const artista = element.artist.id;
+        if (!artistiStampati.has(artista)) {
+          // VERIFICO SE L'ARTISTA è GIA STATO TRACCIATO
+          console.log(artista);
+          artistiStampati.add(artista); //E LO AGGIUNGO AL SET
+        }
+      });
     })
     .catch((err) => {
       console.log(err, "errore");
