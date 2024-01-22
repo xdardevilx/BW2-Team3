8e2202dc12546661fd7e8282d50b2d5a2663af84
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-01-23 00:15:17 +0100

    js artist, sto per iniziare la generazione della pagina in base all artista

--- artist.js
+++ artist.js
@@ -1,10 +1,10 @@
-let myUrl = "https://striveschool-api.herokuapp.com/api/deezer/";
-let nomeArtista = "salmo";
-let searchArtista = "search?q=" + nomeArtista;
+let myUrl = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
+let nomeArtista = "metallica";
 
-const getDataDatas = function () {
-  fetch(myUrl + searchArtista)
+const getArtist = function () {
+  fetch(myUrl + nomeArtista)
     .then((response) => {
+      console.log(myUrl + nomeArtista);
       if (response.ok) {
         return response.json();
       } else {
@@ -12,23 +12,54 @@ const getDataDatas = function () {
       }
     })
     .then((data) => {
-      console.log(data, "data");
-      const oggettoConArray = data;
-      const arrayDiOggetti = oggettoConArray.data;
-      console.log(arrayDiOggetti);
+      console.log("OGGETTO ARTISTA", data);
+      const oggettoArtista = data;
+      const idArtista = oggettoArtista.id;
+      console.log("ID ARTISTA", idArtista);
 
-      const artistiStampati = new Set(); // TRACCIO GLI ARTISTI GIà STAMPATI
-      arrayDiOggetti.forEach((element) => {
-        const artista = element.artist.id;
-        if (!artistiStampati.has(artista)) {
-          // VERIFICO SE L'ARTISTA è GIA STATO TRACCIATO
-          console.log(artista);
-          artistiStampati.add(artista); //E LO AGGIUNGO AL SET
-        }
-      });
+      ////////////// CONTANDO CHE NON DOVREBBERO ESSERCI ARTISTI CON LO STESSO NOME FORSE NON SERVE TROVARE L'OGGETTO
+      ///////////// ARTIST CON ANCHE L 'ID NELL URL, DOVREBBE BASTARE L URL + NOME ARTISTA .. ? MA INTANTO LASCIO ANCHE QUESTA PARTE
+      fetch(myUrl + idArtista)
+        .then((response) => {
+          console.log(myUrl + idArtista);
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("ERRORE");
+          }
+        })
+        .then((dataIdUrl) => {
+          //   const album = data.album;
+          console.log("OGGETTO ARTISTA DA URL CON ID", dataIdUrl);
+        })
+        .catch((err) => {
+          console.log("errore", err);
+        });
+
+      //   const albumStampati = new Set();
+      //   const artistiStampati = new Set(); // TRACCIO GLI ARTISTI GIà STAMPATI
+      //   arrayDiOggetti.forEach((element) => {
+      //     const artista = element.artist.name;
+      // const album = element.album.title;
+
+      // if (!albumStampati.has(album)) {
+      //   // VERIFICO SE L'ARTISTA è GIA STATO TRACCIATO
+      //   console.log(album);
+      //   albumStampati.add(album); //E LO AGGIUNGO AL SET
+      // } else {
+      //   console.log("ALBUM GIà STAMPATO");
+      // }
+      //   });
     })
     .catch((err) => {
       console.log(err, "errore");
     });
 };
-getDataDatas();
+getArtist();
+
+// let searchArtista = nomeArtista;
+
+// search.addEventListener(("click") => {getArtist()})
+
+// URLSearchParams.append() ///////  Aggiunge una coppia chiave/valore specificata come nuovo parametro di ricerca.
+// URLSearchParams.forEach()
