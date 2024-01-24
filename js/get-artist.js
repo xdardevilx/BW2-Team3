46177b092804267961e78c0624fb13f2756a40ee
xdardevilx <valeriogalletti21@gmail.com>
2024-01-24 14:50:21 +0100

    add api search and get

---
+++ js/get-artist.js
@@ -0,0 +1,16 @@
+const url = "https://striveschool-api.herokuapp.com/api/deezer/artist";
+
+export const getArtistAPI = async (idArtist) => {
+  try {
+    const response = await fetch(`${url}/${idArtist}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    } else {
+        const data = await response.json();
+        console.log("DATA", data);
+        return data;
+    }
+  } catch (error) {
+    console.error("Error fetching artist:", error.message);
+  }
+};
