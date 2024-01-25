fbbe407d9eb39c76ad0108200bfadebd4357f053
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-01-25 10:57:29 +0100

    play audio in Artist page da fixare, animazione di caricamento pagina

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -57,6 +57,7 @@ const getArtist = function () {
           ///// PARAMETRIZZATA DAL NUMERO MASSIMO DI TRACKLIST DI OGNI ARTISTA
           console.log("OGGETTO TRACKLIST RICEVUTO DA FETCH", data);
           const arrayTrackList = data.data;
+
           console.log("ARRAY TRACKLIST", arrayTrackList);
 
           const creaTracklist = function () {
@@ -93,7 +94,7 @@ const getArtist = function () {
               track += `
                                 <li class="d-flex align-items-center justify-content-between list-group-item h5"><img src="${
                                   element.album.cover
-                                }" class="col-2 rounded-0 mx-1 my-2 ms-2" alt="..." style="width: 40px; height: 40px;"><span id="titoloPreview" class=" text-white col-6 h4 ms-2 mb-0">${titoloToUpperCaseSenzaParentesi(
+                                }" id="genitore" class="col-2 rounded-0 mx-1 my-2 ms-2" alt="..." style="width: 40px; height: 40px;"><span id="titoloPreview" class=" text-white col-6 h4 ms-2 mb-0">${titoloToUpperCaseSenzaParentesi(
                 element.title
               )}</span><span class="col-2 ms-4 h5 text-info">${formattaNumeroConPunti(
                 element.rank
@@ -102,6 +103,8 @@ const getArtist = function () {
               )}</span></li>
             `;
             });
+
+            hideLoadingAnimation();
 
             divTracklist.innerHTML = track;
 
@@ -126,7 +129,7 @@ const getArtist = function () {
             titoloPreviewElements.forEach((titoloPreviewElement, i) => {
               titoloPreviewElement.addEventListener("click", function () {
                 const isPlaying =
-                  titoloPreviewElement.classList.toggle("active");
+                  titoloPreviewElement.classList.toggle("active"); ////
 
                 if (isPlaying) {
                   titoloPreviewElement.classList.remove("text-white");
@@ -162,6 +165,34 @@ const getArtist = function () {
     .catch((err) => {
       console.log("errore", err);
     });
+
+  function hideLoadingAnimation() {
+    const loadingDiv = document.getElementById("loadingDiv");
+    if (loadingDiv) {
+      loadingDiv.style.display = "none";
+    }
+  }
+
+  // Verifica se il div genitore è stato creato
+  const parentDiv = document.getElementById("genitore");
+
+  // Se il div genitore non è ancora stato creato, creo loading div e mostro l'animazione
+  if (!parentDiv) {
+    const loadingDiv = document.createElement("div");
+    loadingDiv.id = "loadingDiv";
+    loadingDiv.classList.add("clessidra");
+    loadingDiv.style.width = "20px";
+    loadingDiv.style.height = "20px";
+    document.body.appendChild(loadingDiv);
+  } else {
+    hideLoadingAnimation(); // Nascondi l'animazione se il div genitore è già stato creato
+    const loadingDiv = document.getElementById("loadingDiv");
+    // Aggiungi un listener per l'evento "animationend" all'elemento di caricamento
+    loadingDiv.addEventListener("animationend", () => {
+      // Una volta completata l'animazione di opacità, nascondi il div di caricamento
+      loadingDiv.style.display = "none";
+    });
+  }
 };
 getArtist();
 
